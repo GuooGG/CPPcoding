06a66c92e2a7291e9accad57e2e623fe8814c96c
GGo <2311619185@qq.com>
2023-11-13 09:26:46 +0800

    树状数组

--- Algo/BinIndexedTree.cpp
+++ Algo/BinIndexedTree.cpp
@@ -12,37 +12,45 @@
 
 class BinIndexedTree{
 public:
-    
+    void add(int x, int n)
+    {
+        for (int i = x; i <= n; i += lowbit(i))
+        {
+            tree[i] += n;
+        }
+    }
+    int query(int x)
+    {
+        int ans = 0;
+        for (int i = x; i > 0; i -= lowbit(i))
+        {
+            ans += tree[i];
+        }
+        return ans;
+    }
+    BinIndexedTree(std::vector<int> &arr)
+    {
+        this->nums = arr;
+        n = arr.size();
+        tree.resize(n + 1);
+        for (int i = 0; i < n; i++)
+        {
+            add(i + 1, nums[i]);
+        }
+    }
+    std::vector<int> nums;
+
 private:
     int lowbit(int x)
     {
         return x & -x;
     }
-    void add(int x, int n){
-        for(int i = x; i <= n; i += lowbit(i)){
-            tree[i] += n;
-        }
-    }
-    int query(int x){
-        int ans = 0;
-        for(int i = x; i > 0; i -= lowbit(i)){
-            ans += tree[i];
-        }
-        return ans;
-    }
+
     void update(int index, int val){
         add(index + 1, val);
         nums[index] = val;
     }
-    BinIndexedTree(std::vector<int>& arr){
-        this->nums = arr;
-        n = arr.size();
-        tree.resize(n + 1);
-        for(int i = 0; i < n; i++){
-            add(i + 1, nums[i]);
-        }
-    }
+
     std::vector<int> tree;
-    std::vector<int> nums;
     int n = 0;
 };
